3ca536752f22c895ce9e12dfad65f9405c3a3a90
akingayi <akingbayiojo@gmail.com>
2025-02-11 13:17:20 +0100

    Requests and Response

--- modules.js
+++
@@ -1,9 +0,0 @@
-// const xyz = require("./people");
-// const { people, ages } = require("./people");
-
-// console.log(xyz.people, xyz.ages);
-// console.log(people, ages);
-
-const os = require("os"); // os is a built-in module that stands for operating system
-
-console.log(os.platform(), os.homedir());
